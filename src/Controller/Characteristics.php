cd88450bc6fa4041685dcd2766d5b1212d025bba
sebastien deren laptop <sebastien.deren@gmail.com>
2023-08-30 09:26:14 +0200

    adding characteristics and test

--- src/Controller/Characteristics.php
+++ src/Controller/Characteristics.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Domain\Data\CharacteristicsEnum;
+use App\Domain\Personnages\Characteristiques\CharacRules;
 
 class Characteristics
 {
@@ -12,4 +13,46 @@ class Characteristics
     public int $dexterite;
     public int $charisme;
     public int $endurance;
+    private CharacRules $rules;
+    private ?array $dices;
+
+    /**
+     * Get the value of rules
+     */
+    public function getRules()
+    {
+        return $this->rules;
+    }
+
+    /**
+     * Set the value of rules
+     *
+     * @return  self
+     */
+    public function setRules($rules)
+    {
+        $this->rules = $rules;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dices
+     */
+    public function getDices()
+    {
+        return $this->dices;
+    }
+
+    /**
+     * Set the value of dices
+     *
+     * @return  self
+     */
+    public function setDices($dices)
+    {
+        $this->dices = $dices;
+
+        return $this;
+    }
 }
